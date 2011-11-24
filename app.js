7112e0f0570b420d0706481f474f5164f2195d25
Alvaro Oliveira <alvaro.oliveira@gmail.com>
2011-11-23 17:23:37 -0800

    Added express and Socket.IO

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-var express = require('express');
-var app = express.createServer();
-var jqtpl = require('jqtpl');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+
+var app = module.exports = express.createServer();
+
+// Configuration
 
 app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.set('views', __dirname + '/views');
-    app.set('views');
-    app.set('view engine', 'html');
-    app.register('.html', require('jqtpl').express)
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
+  app.use(express.errorHandler()); 
 });
 
-app.get('/', function(req,res){
-  res.render('index');
-})
+// Routes
+
+app.get('/', routes.index);
 
 app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
