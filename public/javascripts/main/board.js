1059dcfd9682db0d39be2c57f8f1edacf58de5fc
Alvaro Oliveira <alvaro.oliveira@gmail.com>
2011-11-24 02:46:27 -0200

    Made initial logic to set ships on the board for one player

--- public/javascripts/main/board.js
+++ public/javascripts/main/board.js
@@ -1,17 +1,40 @@
 var board = function() {
 
+	var playerShips = 5;
+
   function InitializeEvents() {
-    $('#btnAddBlock').click(btnAddBlockClicked);
+    $('#btnClearShips').click(btnClearShipsClicked);
+    $('#btnReady').click(btnReadyClicked);
   }
 
-  function btnAddBlockClicked() {
-    board.AddBlock();
+  function btnReadyClicked() {
+    $("#btnClearShips").hide();
+  }
+  
+  function btnClearShipsClicked() {
+    ResetShipSelection();
+  }
+  
+  function AddShip(){
+    $(this).addClass('playerBlock');
+    playerShips = playerShips - 1;
+    $('#shipsRemaining').text('Ships Remaining ' + playerShips);
+    if(playerShips <= 0)
+      $('.block').off('click', AddShip);
+  }
+  
+  function ResetShipSelection() {
+    $(".playerBlock").removeClass('playerBlock');
+    playerShips = 5;
+    $('#shipsRemaining').text('Ships Remaining ' + playerShips);
+    $('.block').on('click',AddShip);
   }
 
   return {
     Initialize: function() {      
       InitializeEvents();
       this.ResetBoard();
+      this.SelectShipsPosition();
     },
     
     AddBlock: function() {
@@ -21,14 +44,21 @@ var board = function() {
     },
     
     ResetBoard: function() {
-      var total = 16 * 16;
+      var lineTotal = 16;
+      var columnTotal = 16;
       var block = "";
       $board = $('#gameboard');
-      for(i = 0; i < total; i++){
-        block += "<div class='block'></div>";
+      for(i = 0; i < lineTotal; i++) {
+        for(j = 0; j < columnTotal; j++ ) {
+          block += "<div class='block' data-line='" + i + "' data-column='" + j + "'></div>";
+        }
       }
       $board.append(block);
-    }    
+    },
+    
+    SelectShipsPosition: function() {
+      ResetShipSelection();
+    }
   }
 }();
 
